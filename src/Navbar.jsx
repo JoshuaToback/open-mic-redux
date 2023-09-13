1fd942f5e168523ba5782515dc848722cb13fe93
JoshuaToback <joshuatoback@gmail.com>
2023-09-13 17:20:05 -0400

    Added mobile responsive Nav

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,39 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 
 import OMS from "./assets/images/oms_temp.png";
 
 export default function Navbar() {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link activeClass="active" smooth spy to="services">
-            SERVICES
-          </Link>
-        </li>
-        <li>
-          <Link activeClass="active" smooth spy to="clients">
-            CLIENTS
-          </Link>
-        </li>
 
-        <li>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  const menuClass = isMenuOpen ? "menu-links.open" : "menu-links";
+  return (
+    <>
+      <nav id="desktop-nav">
+        <ul className="nav-links">
+          <li>
+            <Link activeClass="active" smooth spy to="services">
+              SERVICES
+            </Link>
+          </li>
+          <li>
+            <Link activeClass="active" smooth spy to="clients">
+              CLIENTS
+            </Link>
+          </li>
+
+          <li>
+            <Link activeClass="active" smooth spy to="home">
+              <img src={OMS} alt="OMS Logo" className="nav-logo" />
+            </Link>
+          </li>
+          <li>
+            <Link activeClass="active" smooth spy to="about">
+              ABOUT US
+            </Link>
+          </li>
+          <li>
+            <Link activeClass="active" smooth spy to="contact">
+              CONTACT
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <nav id="hamburger-nav">
+        <div className="hamburger-items">
           <Link activeClass="active" smooth spy to="home">
             <img src={OMS} alt="OMS Logo" className="nav-logo" />
           </Link>
-        </li>
-        <li>
-          <Link activeClass="active" smooth spy to="about">
-            ABOUT US
-          </Link>
-        </li>
-        <li>
-          <Link activeClass="active" smooth spy to="contact">
-            CONTACT
-          </Link>
-        </li>
-      </ul>
-    </nav>
+          <div className="hamburger-menu">
+            <div
+              className={`hamburger-icon ${isMenuOpen ? "open" : ""}`}
+              onClick={toggleMenu}
+            >
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <ul className={menuClass}>
+              <li>
+                <Link
+                  activeClass="active"
+                  smooth
+                  spy
+                  to="services"
+                  onClick={toggleMenu}
+                >
+                  SERVICES
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass="active"
+                  smooth
+                  spy
+                  to="clients"
+                  onClick={toggleMenu}
+                >
+                  CLIENTS
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass="active"
+                  smooth
+                  spy
+                  to="about"
+                  onClick={toggleMenu}
+                >
+                  ABOUT US
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass="active"
+                  smooth
+                  spy
+                  to="contact"
+                  onClick={toggleMenu}
+                >
+                  CONTACT
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
